ebccff071676da34b92ee7718256772cfde37c97
Yuya Hattori <y.hattori@grander.jp>
2023-09-21 15:43:02 +0900

    Vite開発環境を導入

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
             input: [
