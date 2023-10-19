a85b664ed53570888d45b9c9220ef5190b475777
Yuya Hattori <y.hattori@grander.jp>
2023-10-19 16:37:46 +0900

    API用FromRequestクラス追加に伴うディレクトリ構成及び名前空間の変更

--- src/app/Http/Controllers/Web/ProfileController.php
+++ src/app/Http/Controllers/Web/ProfileController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ProfileUpdateRequest;
+use App\Http\Requests\Web\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
