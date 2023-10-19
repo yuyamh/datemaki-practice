d475fa70a0855e513dfc0596d71c526100b77234
Yuya Hattori <y.hattori@grander.jp>
2023-10-19 17:36:03 +0900

    API用FormRequestクラスの作成

---
+++ src/app/Http/Requests/Api/PostFormRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class PostFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $rules = [
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'level' => ['required', Rule::in(['A1', 'A2', 'B1', 'B2', 'C1', 'C2'])],
+            'file_name' => [
+                'file',
+                // 拡張子
+                'mimes:pdf,docx,zip,xlsx,jpeg,jpg,png',
+                // MIMEタイプ：Word, Excel, Zip, PDF
+                'mimetypes:application/pdf,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/zip,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,image/jpeg,image/png',
+            ],
+            'text_id' => 'nullable|exists:texts,id',
+        ];
+
+        return $rules;
+    }
+
+    // API用のエラーレスポンスを返す
+    protected function failedValidation(Validator $validator)
+    {
+        $res = response()->json([
+            'errors' => $validator->errors(),
+            ],
+            400);
+            
+        throw new HttpResponseException($res);
+    }
+}
