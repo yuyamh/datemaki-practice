b262c34c53ad4942b1ff8dcf73edb2d38a55c9bd
Yuya Hattori <y.hattori@grander.jp>
2023-10-23 13:46:46 +0900

    API用ブックマーク処理のコントローラの作成

---
+++ src/app/Http/Controllers/Api/BookmarkController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Bookmark;
+use Illuminate\Http\Request;
+
+class BookmarkController extends Controller
+{
+
+    /**
+     * ブックマークを追加する
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * ブックマークを削除する
+     */
+    public function destroy(Bookmark $bookmark)
+    {
+        //
+    }
+}
