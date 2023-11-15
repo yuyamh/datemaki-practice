9a0d1c47766816fcea3030dafb916ffaa896a539
Yuya Hattori <y.hattori@grander.jp>
2023-11-15 16:20:48 +0900

    タスクスケジューラの設定

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -2,11 +2,22 @@
 
 namespace App\Console;
 
+use app\Console\Commands\DeleteUnnecessaryUploadedFiles;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        DeleteUnnecessaryUploadedFiles::class,
+    ];
+
     /**
      * Define the application's command schedule.
      *
@@ -15,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('command:deleteFiles')->daily();
     }
 
     /**
